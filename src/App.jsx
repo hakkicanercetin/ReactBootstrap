c0638e7ab1ffeb98562ecc76275e0d66a59dbde7
Hakkıcan ERÇETİN <hakkicanercetin@gmail.com>
2024-01-06 16:26:55 +0300

    bootstrap js added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HeroDiv } from "./sections/hero/Hero";
 import { Navbar } from "./sections/navbar/Navbar";
 import { Footer } from "./sections/footer/Footer";
 import { Features } from "./sections/features/features";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
 function App() {
 
   return (
