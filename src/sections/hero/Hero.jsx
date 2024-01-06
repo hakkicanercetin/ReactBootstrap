7295ac17790b071c38af1f275c68806f9b3f59c2
Hakkıcan ERÇETİN <hakkicanercetin@gmail.com>
2024-01-06 16:17:41 +0300

    Year updated again.

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -7,7 +7,7 @@ export function HeroDiv()
                 <div className="p-4 p-lg-5 text-center bg-light rounded-3">
                     <div className="m-4 m-lg-5">
                         <h1 className="fw-bold fs-1">React.js</h1>
-                        <p className="fs-4">2011 yılında Facebook tarafından geliştirilen, kullanıcı arayüzü oluşturmaya yarayan, açık kaynak kodlu bir Javascript kütüphanesidir.React, birçok kişi tarafından tercih edilen popüler bir JavaScript kütüphanesidir. <a href="https://survey.stackoverflow.co/2022/#web-frameworks-and-technologies">Stack Overflow 2022 anketine</a> göre en sevilen ikinci kütüphanedir. </p>
+                        <p className="fs-4">2011 yılında Facebook tarafından geliştirilen, kullanıcı arayüzü oluşturmaya yarayan, açık kaynak kodlu bir Javascript kütüphanesidir.React, birçok kişi tarafından tercih edilen popüler bir JavaScript kütüphanesidir. <a href="https://survey.stackoverflow.co/2022/#web-frameworks-and-technologies">Stack Overflow 2023 anketine</a> göre en sevilen ikinci kütüphanedir. </p>
                         <a className="btn btn-primary btn-lg" href="#">Call to action</a>
                     </div>
                 </div>
