c731461e34ca174935b305e67fc68e6bc569fdc6
Hakkıcan ERÇETİN <38086532+hakkicanercetin@users.noreply.github.com>
2024-01-28 10:29:10 +0000

    update

--- src/components/IconBox/Icon.jsx
+++ src/components/IconBox/Icon.jsx
@@ -1,10 +1,9 @@
 import styles from "./Iconbox.module.scss";
-export function IconBox(/* icon */)
+export function IconBox(icon)
 {
     return (
         <div className={styles.feature + " bg-primary text-white rounded-3 mb-4"}>
-      {/* <i className={icon.icon}></i> */}
-      Merhaba
+      <i className={icon.icon}></i>
       </div>
     );
 }
