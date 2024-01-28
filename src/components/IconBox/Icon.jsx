d3f32a7d3028b2b6cce3b437490c81e330fb2232
Hakkıcan ERÇETİN <38086532+hakkicanercetin@users.noreply.github.com>
2024-01-28 10:27:42 +0000

    update

--- src/components/IconBox/Icon.jsx
+++ src/components/IconBox/Icon.jsx
@@ -4,6 +4,7 @@ export function IconBox(/* icon */)
     return (
         <div className={styles.feature + " bg-primary text-white rounded-3 mb-4"}>
       {/* <i className={icon.icon}></i> */}
+      Merhaba
       </div>
     );
 }
